7a209fbe3ad668805b1c54ce5c3d945084bb20e7
kassent <kassent@outlook.com>
2017-12-12 23:09:17 +0800

    v1.0.4

--- Achievements/main.cpp
+++ Achievements/main.cpp
@@ -10,38 +10,38 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD reason, LPVOID reserved)
 	{
 		gLog.OpenRelative(CSIDL_MYDOCUMENTS, "\\Larian Studios\\Divinity Original Sin 2\\NativeModLogs\\Achievements.log");
 		uintptr_t patchIndex = 0;
-		try
-		{
-			uintptr_t patchA = RVAScan<uintptr_t>("75 3E 48 8B 8B ? ? ? ?").GetUIntPtr();//0x9090
-			SafeWrite16(patchA, 0x9090);
-			_MESSAGE("(%016I64X) patched sucessfully...", patchA - RelocationManager::s_baseAddr);
-		}
-		catch (const no_result_exception & exceptioin)
-		{
-			_MESSAGE("[%d]%s", ++patchIndex, exceptioin.what());
-		}
+		//try
+		//{
+		//	uintptr_t patchA = RVAScan<uintptr_t>("75 3E 48 8B 8B ? ? ? ?").GetUIntPtr();//0x9090
+		//	SafeWrite16(patchA, 0x9090);
+		//	_MESSAGE("(%016I64X) patched sucessfully...", patchA - RelocationManager::s_baseAddr);
+		//}
+		//catch (const no_result_exception & exceptioin)
+		//{
+		//	_MESSAGE("[%d]%s", ++patchIndex, exceptioin.what());
+		//}
 
-		try
-		{
-			uintptr_t patchB = RVAScan<uintptr_t>("75 3F 48 8B 8B ? ? ? ?").GetUIntPtr();
-			SafeWrite16(patchB, 0x9090);
-			_MESSAGE("(%016I64X) patched sucessfully...", patchB - RelocationManager::s_baseAddr);
-		}
-		catch (const no_result_exception & exceptioin)
-		{
-			_MESSAGE("[%d]%s", ++patchIndex, exceptioin.what());
-		}
+		//try
+		//{
+		//	uintptr_t patchB = RVAScan<uintptr_t>("75 3F 48 8B 8B ? ? ? ?").GetUIntPtr();
+		//	SafeWrite16(patchB, 0x9090);
+		//	_MESSAGE("(%016I64X) patched sucessfully...", patchB - RelocationManager::s_baseAddr);
+		//}
+		//catch (const no_result_exception & exceptioin)
+		//{
+		//	_MESSAGE("[%d]%s", ++patchIndex, exceptioin.what());
+		//}
 
-		try
-		{
-			uintptr_t patchC = RVAScan<uintptr_t>("74 08 32 C0 48 83 C4 20 5B C3 48 8B 53 08").GetUIntPtr(); //74->EB
-			SafeWrite8(patchC, 0xEB);
-			_MESSAGE("(%016I64X) patched sucessfully...", patchC - RelocationManager::s_baseAddr);
-		}
-		catch (const no_result_exception & exceptioin)
-		{
-			_MESSAGE("[%d]%s", ++patchIndex, exceptioin.what());
-		}
+		//try
+		//{
+		//	uintptr_t patchC = RVAScan<uintptr_t>("74 08 32 C0 48 83 C4 20 5B C3 48 8B 53 08").GetUIntPtr(); //74->EB //74 08 32 C0 48 83 C4 20 5F C3 48 89 5C 24 ? 48 8D 4F 10
+		//	SafeWrite8(patchC, 0xEB);
+		//	_MESSAGE("(%016I64X) patched sucessfully...", patchC - RelocationManager::s_baseAddr);
+		//}
+		//catch (const no_result_exception & exceptioin)
+		//{
+		//	_MESSAGE("[%d]%s", ++patchIndex, exceptioin.what());
+		//}
 		//patchD
 		//v3.0.150.188
 		try
